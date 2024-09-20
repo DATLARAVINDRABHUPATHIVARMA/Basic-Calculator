11b4110d9cc89227e71ae51a88c5035b83df5ddf
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2024-09-20 00:37:16 -0600

    Asp .net

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -9,4 +9,4 @@ const ButtonsContainer = () => {
 };
 
 export default ButtonsContainer;
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis.
