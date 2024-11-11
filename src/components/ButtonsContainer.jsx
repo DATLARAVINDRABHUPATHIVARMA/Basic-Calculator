9e907ad0cc5cbca0cf0eafb7083af8ace41164dc
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2024-11-11 02:03:03 -0600

    Invoice

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -9,4 +9,4 @@ const ButtonsContainer = () => {
 };
 
 export default ButtonsContainer;
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid architecto iste sint non, tenetur similique possimus voluptate.
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid architecto iste sint non, tenetur similique possimus.
