b745b67e260c5153805a907580c639912c61c17f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2024-11-15 02:07:16 -0600

    Code issues

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -9,4 +9,4 @@ const ButtonsContainer = () => {
 };
 
 export default ButtonsContainer;
-Lorem ipsum dolor sit amet consectetur adipisicing ElementInternals.
+Lorem ipsum dolor sit amet consectetur adipisice.
