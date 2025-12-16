30128418bead73c143b0491dca824f81f035949f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-12-15 22:47:53 -0800

    React Calculator work

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -2,6 +2,9 @@ const ButtonsContainer = () => {
   return (
     <div className="buttons-container">
       <button className="button">C</button>
+      <button className="button">C</button>
+      <button className="button">C</button>
+      <button className="button">C</button>
     </div>
   );
 };
