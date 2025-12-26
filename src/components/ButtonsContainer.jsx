6082d93cb95380bbdc5d11561c65cc82fb66e24a
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-12-26 04:34:53 -0600

    Master branch

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -2,7 +2,7 @@ const ButtonsContainer = () => {
   return (
     <div className="buttons-container">
       <button className="button">C</button>
-      <button className="button">%</button>
+      <button className="button">CC</button>
     </div>
   );
 };
