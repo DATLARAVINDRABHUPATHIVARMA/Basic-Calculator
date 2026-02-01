d3065aa1d530e6136b8cff88f7af9bb0132ad07c
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-02-01 15:35:38 +0530

    Code Conditionality

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,6 +1,5 @@
 const ButtonsContainer = () => {
   const buttonNames = ["AC", "C", "%", "^", "!", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "=", "e", "(", ")", "sin", "cos", "tan", "log", "sqrt", "pi"];
-
   return (
     <div className="buttons-container">
       {buttonNames.map((name) => <button key={name} className="button">{name}</button>)}
@@ -9,4 +8,3 @@ const ButtonsContainer = () => {
 };
 
 export default ButtonsContainer;
-dfs
