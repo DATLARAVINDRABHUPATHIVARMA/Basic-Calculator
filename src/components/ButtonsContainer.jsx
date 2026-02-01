548a605f49124ec8a5f57cb62150b00187de8d86
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-02-01 04:08:06 -0600

    Style Alignment

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,5 +1,6 @@
 const ButtonsContainer = () => {
   const buttonNames = ["AC", "C", "%", "^", "!", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "=", "e", "(", ")", "sin", "cos", "tan", "log", "sqrt", "pi"];
+  
   return (
     <div className="buttons-container">
       {buttonNames.map((name) => <button key={name} className="button">{name}</button>)}
